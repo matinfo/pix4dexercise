196026bbdb4957ddb6f016783f2cb71936428fb0
Mathieu Meylan <m.meylan@gmail.com>
2016-02-28 15:00:26 +0100

    debug on for heroku

--- settings/heroku.py
+++ settings/heroku.py
@@ -18,6 +18,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = False
+DEBUG = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
